9b08bf844c88d9bdbe4ba8b2e9602f26fdb17938
yanixxx <91158657+yanixxx@users.noreply.github.com>
2021-12-07 15:18:27 +0300

    Add files via upload

---
+++ 3pr/9pr.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def s_f():
+    sum=0
+    N = int(input('N: '))
+    past_num = 0
+    present_num = 1
+    for i in range(2, N+1):
+        sum += present_num
+        time = present_num
+        present_num += past_num
+        past_num = time
+
+    return sum
+print(s_f())
